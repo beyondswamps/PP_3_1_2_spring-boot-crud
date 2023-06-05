3cda9e3c85b69c4e5141510728a6eaff1289f2fa
Oleg Zabolotnykh <beyondswamps@gmail.com>
2023-06-05 10:53:00 +0300

    back from EntityManagerFactory to EntityManager

--- src/main/java/net/olega/springcrud/PP_3_1_2_springbootcrud/dao/UserDaoImp.java
+++ src/main/java/net/olega/springcrud/PP_3_1_2_springbootcrud/dao/UserDaoImp.java
@@ -1,10 +1,8 @@
 package net.olega.springcrud.PP_3_1_2_springbootcrud.dao;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
 import net.olega.springcrud.PP_3_1_2_springbootcrud.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,26 +11,24 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-//    @PersistenceContext
-    final private EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    final private EntityManager entityManager;
 
-    public UserDaoImp(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
+    public UserDaoImp(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @Override
     @Transactional
     public void addUser(User user) {
-        entityManagerFactory
-                .createEntityManager()
+        entityManager
                 .persist(user);
     }
 
     @Override
     @Transactional
     public List<User> getUsers() {
-        return entityManagerFactory
-                .createEntityManager()
+        return entityManager
                 .createQuery("from User", User.class)
                 .getResultList();
     }
@@ -40,24 +36,21 @@ public class UserDaoImp implements UserDao {
     @Override
     @Transactional
     public User getUser(Long id) {
-        return entityManagerFactory
-                .createEntityManager()
+        return entityManager
                 .find(User.class, id);
     }
 
     @Override
     @Transactional
     public void updateUser(User user) {
-        entityManagerFactory
-                .createEntityManager()
+        entityManager
                 .merge(user);
     }
 
     @Transactional
     @Override
     public void deleteUser(Long id) {
-        entityManagerFactory
-                .createEntityManager()
+        entityManager
                 .createQuery("delete from User where id = :id")
                 .setParameter("id", id)
                 .executeUpdate();
